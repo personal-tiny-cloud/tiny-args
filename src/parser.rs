9833da0bc06f042f90f561e9324cef6d9992c9fe
hex0x0000 <hex0x0000@protonmail.com>
2024-08-07 04:14:14 +0200

    Added license notice
    Signed-off-by: hex0x0000 <hex0x0000@protonmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,24 @@
+// This file is part of the Tiny Cloud project.
+// You can find the source code of every repository here:
+//		https://github.com/personal-tiny-cloud
+//
+// Copyright (C) 2024  hex0x0000
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Email: hex0x0000@protonmail.com
+
 use crate::*;
 
 fn args(
